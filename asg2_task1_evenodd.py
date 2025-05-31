f2e89ec6e370568dafb3aa0aaae80af085da5299
Swapnil8483 <swapnilmore932@gmail.com>
2025-05-31 18:45:48 +0530

    Add files via upload

---
+++ asg2_task1_evenodd.py
@@ -0,0 +1,7 @@
+print("Give me a number and I'll check if it's even or odd.")
+num = int(input("Enter a number :"))
+
+if num % 2 != 0:
+    print(f"{num} is an odd number.")
+else:
+    print(f"{num} is an even number.")
