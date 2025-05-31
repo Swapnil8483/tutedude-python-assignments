1267016ff01d815f09338bcd164e38e55bb6f374
Swapnil8483 <swapnilmore932@gmail.com>
2025-05-31 18:46:28 +0530

    Add files via upload

---
+++ asg2_task2_sum_n_numbers.py
@@ -0,0 +1,17 @@
+num = 0
+for i in range(1,51):
+    num = num + i
+
+print (f"The sum of numbers from 1 to 50 is : {num}")
+
+
+'''
+For sum of 1 to n number
+
+i = int(input("Enter the number upto which you want to calculate sum, from 1 :"))
+num = 0
+
+for x in range (1, i+1):
+    num = num + x
+print (f"The sum of numbers from 1 to 50 is : {num}")
+'''
