055df06913d392644a0dc01b9c2520067387e0ed
Swapnil8483 <swapnilmore932@gmail.com>
2025-06-03 15:40:04 +0530

    Add files via upload

---
+++ asg5_task2.py
@@ -0,0 +1,7 @@
+lst_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ext_num = lst_num[0 : 5]
+rev_num = list(reversed(ext_num))
+
+print(f"Original list: {lst_num}")
+print(f"Extracted first 5 elements : {ext_num}")
+print(f"Reversed Extracted elements : {rev_num}")
